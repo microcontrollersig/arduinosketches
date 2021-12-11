0232407c093ec9b9adceee4b637d5f60a7a62f3a
microcontrollersig <melbpc.computerb@gmail.com>
2021-12-11 13:56:58 +1100

    Add files via upload

---
+++ MohanOledTest.ino
@@ -0,0 +1,62 @@
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+
+
+void drawtext(int x, int y, String text) {
+  display.setCursor(x, y);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.print(text);       // our display****
+  display.display(); 
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  // Clear the buffer
+  display.clearDisplay();
+ // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+/*  display.setCursor(20, 10);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.print("RPM ");       // our display****
+  display.display(); 
+*/
+ drawtext(20,10,"RPM:  ");  
+ drawtext(55,10,String(10005));
+ drawtext(20,20,"TEMP:  ");  
+ drawtext(55,20,String(20.5));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
